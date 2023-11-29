4cf5f1a5eb4738b50b67786cad79179d85ab1c16
quentindubourgdeluzencon <44379267+quentindubourgdeluzencon@users.noreply.github.com>
2023-11-29 14:30:32 +0100

    Intégration de la correction des exercices

--- Exercice 1 - find, insert, update, remove/2. irons.js
+++ Exercice 1 - find, insert, update, remove/2. irons.js
@@ -1,13 +1,38 @@
 
 // 1 Donnez tous les métaux de marque Philips ou Tefal qui coûtent 30 euros ou moins, qui sont rouges, ont une puissance de 2000 ou
 // plus et disposent d'un espace de rangement pour le cordon et d'une fonction spray.
-
+db.irons.find({
+    $and: [
+        { brand: { $in: ["Philips", "Tefal"] } },
+        { price: { $lte: 30 } },
+        { "description.color": /.*red.*/i },
+        { "technically.power": { $gte: 2000 } },
+        { extras: { $in: ["cord storage space", "spray function"] } }
+    ]
+})
 
 // Donnez le modèle, le prix et la marque du métal le plus cher qui n'est pas rouge, pesant moins de 2 kilos, qui
 // a au moins 2 points positifs et une hauteur maximale de 20 centimètres.
+db.irons.find(
+    {
+        $and: [
+            { "description.color": { $not: /.*red.*/i } },
+            { "technically.weight": { $lt: 2 } },
+            { $where: "this.reviews.plus.length>2" },
+            { "measures.height": { $lte: 20 } },
+            { extras: { $ne: "chord storage space" } }
+        ]
+    },
+    { model: true, price: true, brand: true }
+).sort({ price: -1 }).limit(1)
+
 
 // 3 Faites une boucle à travers les métaux. Augmentez le prix de tous les métaux de 10 %.
-
+var irons = db.irons.find()
+irons.forEach(iron => {
+    var newPrice = iron.price * 1.1;
+    db.irons.updateOne({ _id: iron._id }, { $set: { price: newPrice } })
+})
 
 // 4 Pendant les soldes, chaque marque accorde une remise spécifique.
 // Les remises sont données sous forme d'objet littéral
@@ -15,4 +40,9 @@
 // Mettez à jour tous les prix des métaux.
 // Remarque : pour parcourir le littéral objet des remises, vous pouvez utiliser le code suivant
 // `for (var key in discounts) { … }`
+var discounts = { 'Calor': 1, 'Tefal': 5, 'Philips': 3 }
+for (var brandName in discounts) {
+    var priceIncrease = discounts[brandName] * -1;
+    db.irons.updateMany({ brand: brandName }, { $inc: { price: priceIncrease } })
+}
 
--- Exercice 2 - aggregate/2. cheeses.js
+++ Exercice 2 - aggregate/2. cheeses.js
@@ -1,4 +1,15 @@
 //1 Précisez le nombre de fromages par type de fromage. Mettez le type de fromage en majuscules.
-
+db.cheeses.aggregate([
+    { $project: { type: "$properties.cheesetype", _id: false } }, //project to only cheesetype
+    { $group: { _id: "$type", count: { $sum: 1 } } }, //group by
+    { $project: { cheesetype: { $toUpper: "$_id" }, numberOfCheeses: "$count", _id: false } } //format
+])
 
 //2 Donnez le nombre de fromages par fromagerie
+db.cheeses.aggregate([
+    { $project: { factory: true, variants: true, name: true, _id: false } },
+    { $unwind: "$variants" },
+    { $group: { _id: { factory: "$factory", cheese: "$name" }, numberOfVariantsPerFactoryPerCheese: { $sum: 1 } } },
+    { $match: { numberOfVariantsPerFactoryPerCheese: { $gte: 2 } } },
+    { $group: { _id: "$_id.factory", numberOfCheesesWithAtLeast2Variants: { $sum: 1 } } },
+])
